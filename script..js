13024e6bbf18bc0313d8fe53ed77c024be796891
MarioCabral <mariocabral.a@gmail.com>
2022-06-15 15:48:28 -0300

    commit inicial

---
+++ script..js
@@ -0,0 +1,38 @@
+document.querySelector('.toggle').onclick = function()
+{
+    // OBS: O TOGGLE É UMA CLASSLIST QUE FUNCIONA COMO BOOLEAN( TRUE OR FALSE )!
+    document.querySelector('.toggle').classList.toggle('active'); //MUDANÇA DO ÍCONE SUPERIOR
+    document.querySelector('.menu').classList.toggle('active'); //MUDANÇA DO BACKGROUND
+}
+
+document.querySelector('.home_menu').onclick = function()
+{
+    document.querySelector('.menu').classList.toggle('active'); //MUDANÇA DO ÍCONE SUPERIOR
+    document.querySelector('.toggle').classList.toggle('active'); //MUDANÇA DO BACKGROUND
+}
+
+document.querySelector('.servico_menu').onclick = function()
+{
+    document.querySelector('.menu').classList.toggle('active'); //MUDANÇA DO ÍCONE SUPERIOR
+    document.querySelector('.toggle').classList.toggle('active'); //MUDANÇA DO BACKGROUND
+}
+
+document.querySelector('.sobre_menu').onclick = function()
+{
+    document.querySelector('.menu').classList.toggle('active'); //MUDANÇA DO ÍCONE SUPERIOR
+    document.querySelector('.toggle').classList.toggle('active'); //MUDANÇA DO BACKGROUND
+}
+
+document.querySelector('.contato_menu').onclick = function()
+{
+    document.querySelector('.menu').classList.toggle('active'); //MUDANÇA DO ÍCONE SUPERIOR
+    document.querySelector('.toggle').classList.toggle('active'); //MUDANÇA DO BACKGROUND
+}
+
+document.querySelector('.equipe_menu').onclick = function()
+{
+    document.querySelector('.menu').classList.toggle('active'); //MUDANÇA DO ÍCONE SUPERIOR
+    document.querySelector('.toggle').classList.toggle('active'); //MUDANÇA DO BACKGROUND
+}
+
+
